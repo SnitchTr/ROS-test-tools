9b3a1825b9591bf0d0fc518c7f71125c7c6a3f8d
ArnauOrtega <62354394+ArnauOrtega@users.noreply.github.com>
2021-05-10 09:35:47 +0200

    New and improved keyboard test tool
    It allows to set and change data types of the messages sent

--- test_pkg/test_scripts/keyboard.py
+++ test_pkg/test_scripts/keyboard.py
@@ -2,77 +2,165 @@
 
 import rospy
 import numpy
-
-# import the ROS String message type
-from std_msgs.msg import Int8
-from std_msgs.msg import String
-from std_msgs.msg import Empty
-from test_msgs.msg import test
-
 class KeyboardPublisherNode:
     def __init__(self):
         rospy.loginfo("Starting keyboard")
 
-        # create a publisher object to send dat
-
-        PUBLISH_RATE = 10.0 # 10 Hz
-        # create a timer that calls the timer_callback function at
-        # the specified rate
-        rospy.Timer(rospy.Duration(1.0/PUBLISH_RATE), self.get_input)
+        rospy.loginfo('Starting keyboard... \nFor intructions on how to use the keyboard node type "help"')
+        self.supported_msgs=['Bool','Empty','String','ColorRGBA','Int8','Int16','Int32','Int64','UInt8','UInt16','UInt32','UInt64','Float32','Float64']#,'Int8MultiArray','Int16MultiArray','Int32MultiArray','Int64MultiArray','UInt8MultiArray','UInt16MultiArray','UInt32MultiArray','UInt64MultiArray','Float32MultiArray','Float64MultiArray']
+        self.int_msgs=['Int16','Int32','Int64','Int8','UInt16','UInt32','UInt64','UInt8']
+        self.float_msgs=['Float32','Float64']
+        self.float_array_msgs=['Float32MultiArray','Float64MultiArray']
+        self.int_array_msgs=['Int16MultiArray','Int32MultiArray','Int64MultiArray','Int8MultiArray','UInt16MultiArray','UInt32MultiArray','UInt64MultiArray','UInt8MultiArray']
         self.stable = False
-        self.array = False
+        self.publishable = True
+
+        PUBLISH_RATE = 10.0 #10hz
+        rospy.Timer(rospy.Duration(1.0/PUBLISH_RATE), self.get_input)
     def get_input(self, event):
-        # this function is called by the timer
-        str = input('Enter message:')
-        if str == 'instructions':
-            instructions = test()
-            self.topic = 'instructions'
-            rospy.loginfo('now you will be asked for directions(0 stop,1 foward,2 back,3 right,4 left) type "done" when finished')
-            instructions.direction = self.get_array();
-            rospy.loginfo('now you will be asked for distance in mm of the directions type "done" when finished')
-            instructions.distance = self.get_array();
-            rospy.Publisher(self.topic, test, queue_size=10).publish(instructions)
-            return
-        if str == 'empty':
-            self.topic = input('Enter topic:')
-            rospy.Publisher(self.topic, Empty, queue_size=10).publish()
-            return
+        if not self.stable:
+            self.data = self.get_data()
+            self.topic = self.get_topic()
+        message = self.get_message(data=self.data)
+        if self.publishable:
+            info = 'Data:' + self.data + ' Topic:' + self.topic + ' Message:' + str(message)
+            rospy.loginfo(info)
+            rospy.Publisher(self.topic, self.imported, queue_size=10).publish(message)
+        self.publishable = True
 
-        if str == 'change':
-            self.stable = False
-            str = input('Enter message:')
-        if self.stable == False:
-            self.topic = input('Enter topic:')
-            if self.topic == 'keep':
-                self.topic = input('this will make the topic unchangable write "cancel" to cancel: ')
-                if self.topic == 'cancel':
-                    self.topic = input('Enter topic:')
-                else:
-                    self.stable = True
-        # loginfo to print the string to the terminal
-        try:
-            num = int(str)
-            rospy.loginfo(num)
-            rospy.Publisher(self.topic, Int8, queue_size=10).publish(num)
-
-        # publish the string message
-        except:
-            rospy.loginfo(str)
-            rospy.Publisher(self.topic, String, queue_size=10).publish(str)
-
-    def get_array(self):
-        x = [1]
+    def get_data(self):
+        print("Now you'll be asked to enter a message type. If this is a part of the std_msgs pkg enter the message type direcly otherwise type "+'"other".If you want to keep the message type type "keep" first\nFor help on data types type "help"')
         while 1:
-            inp = input('Enter number:')
-            if inp == 'done':
-                return x
-            try:
-                inta = int(inp)
-            except:
-                rospy.loginfo('Invalid number try again')
+            data_type = input('Enter message type:')
+            package = 'std_msgs.msg'
+            if data_type == 'keep':
+                self.stable = True
                 continue
-            x.append(inta)
-
+            if data_type == 'help':
+                for type in self.supported_msgs:
+                    print(type)
+                print('For message definitions go to http://wiki.ros.org/std_msgs')
+                continue
+            if data_type == 'other':
+                package = input('Enter pkg name where the msg is stored:') + '.msg'
+                data_type = input('Enter message type:')
+            if data_type in self.supported_msgs:
+                self.imported = getattr(__import__ (package,fromlist=[data_type]),data_type)
+                data = data_type
+                return data
+            else:
+                print('Enter a valid type')
+    def get_topic(self):
+        print("Now you'll be asked to enter a topic to publish your message to."+'If you want to keep the topic type "keep" first')
+        while 1:
+            topic = input('Enter topic:')
+            if topic == 'keep':
+                self.stable = True
+                continue
+            else:
+                return topic
+    def get_message(self,data):
+        while 1:
+            if data in self.float_array_msgs or data in self.int_array_msgs:
+                print("You'll be asked to enter a number to add to the array." + ' Type "done" when finished. The minimum amount of values on the array is 1')
+            if data == 'Empty':
+                print("The empty message doesn't contain any data so you can write whatever you can")
+            if data == 'ColorRGBA':
+                print("You'll be asked to enter 4 float values corresponding to the Red, Green, Blue and Alpha chanel values in this order")
+            if data == 'Bool':
+                print('For the Bool message write 0 for False and 1 for True')
+            msg = input('Enter message:')
+            if msg == 'change':
+                self.stable = False
+                self.publishable = False
+                return
+            if data == 'String':
+                return msg
+            if data in self.int_msgs:
+                try:
+                    num = int(msg)
+                    return num
+                except:
+                    print('Enter a valid message')
+            if data in self.float_msgs:
+                try:
+                    num = float(msg)
+                    return num
+                except:
+                    print('Enter a valid message')
+                    continue
+            if data in self.float_array_msgs:
+                x = [1]
+                try:
+                    inta = float(msg)
+                except:
+                    rospy.loginfo('Invalid number try again')
+                    continue
+                x.append(inta)
+                while 1:
+                    inp = input('Enter number:')
+                    if inp == 'done':
+                        return x
+                    try:
+                        inta = float(inp)
+                    except:
+                        rospy.loginfo('Invalid number try again')
+                        continue
+                    x.append(inta)
+            if data in self.int_array_msgs:
+                x = [1]
+                try:
+                    inta = int(msg)
+                except:
+                    rospy.loginfo('Invalid number try again')
+                    continue
+                x.append(inta)
+                while 1:
+                    inp = input('Enter number:')
+                    if inp == 'done':
+                        return x
+                    try:
+                        inta = int(inp)
+                    except:
+                        rospy.loginfo('Invalid number try again')
+                        continue
+                    x.append(inta)
+            if data == 'Bool':
+                if msg == '0' or msg== '1':
+                    boolean = bool(int(msg))
+                    return boolean
+                else:
+                    print('Enter a valid message')
+                    continue
+            if data == 'ColorRGBA':
+                color = self.imported()
+                try:
+                    num = float(msg)
+                except:
+                    print('Enter a valid message')
+                color.r = num
+                msg = input('Enter message:')
+                try:
+                    num = float(msg)
+                except:
+                    print('Enter a valid message')
+                color.g = num
+                msg = input('Enter message:')
+                try:
+                    num = float(msg)
+                except:
+                    print('Enter a valid message')
+                color.b = num
+                msg = input('Enter message:')
+                try:
+                    num = float(msg)
+                except:
+                    print('Enter a valid message')
+                color.a = num
+                return color
+            else:
+                print('unexpected error not available data type and topic change it by typing "change"')
+                continue
 if __name__ == "__main__":
     rospy.init_node("keyboard")
     node = KeyboardPublisherNode()
